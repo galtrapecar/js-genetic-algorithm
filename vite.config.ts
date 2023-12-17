65a73f710ffee36eef14e53f30b4e1df10636510
Gal Trapečar <trapecar.gal@gmail.com>
2023-12-17 22:34:38 +0100

    single html file build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   clearScreen: false,
   server: {
     port: 8000,
